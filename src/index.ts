ce86d9861df7b336a9d3617301afcc66aa6a27dd
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-06-22 16:25:51 +0200

    cookie for one min

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.use(
     cookie: {
       sameSite: false,
       secure: true,
-      maxAge: 1000 * 60 * 1 * 1, // One minute
+      maxAge: 1000 * 60 * 60 * 1, // One minute
     },
   })
 );

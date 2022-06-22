10f49ab0f1366965e1c9362bc0e394bd7adb6b15
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-06-22 13:57:01 +0200

    change cokie for 1 minute

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.use(
     cookie: {
       sameSite: "none",
       secure: true,
-      maxAge: 1000 * 60 * 60 * 1, // One hour
+      maxAge: 1000 * 60 * 1 * 1, // One minute
     },
   })
 );
